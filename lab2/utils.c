f1224baea884db48b0da661cbdfb4311b756b554
Tiago Pinto <up202206280@up.pt>
2024-02-24 12:35:46 +0000

    Util.c Finished

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,21 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *lsb=(u_int8_t)(val & 0xFF);
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *msb=(u_int8_t)((val >>8)& 0xFF);
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (value == NULL)
+    return 1;
+  uint32_t temp;
+  if(sys_inb(port,&temp)!=0)
+    return 1;
+  *value=(u_int8_t)temp;
+  return 0;
 }
