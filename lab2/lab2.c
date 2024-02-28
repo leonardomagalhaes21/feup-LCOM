b5c25bd67eba50e4099280f3a836e2d9c7ca850d
Tiago Pinto <up202206280@up.pt>
2024-02-28 22:17:55 +0000

    2 functions of lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -30,22 +30,22 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
+  u_int8_t temp;
+  if(timer_get_conf(timer, &temp)==0){
+    if(timer_display_conf(timer, temp,field)==0){
+      return 0;
+    }
+  }
   return 1;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
+  if(timer_set_frequency(timer,freq)==0)return 0;
   return 1;
 }
 
 int(timer_test_int)(uint8_t time) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
 
   return 1;
 }
--- lab2/timer.c
+++ lab2/timer.c
@@ -9,10 +9,15 @@ int hookId=0;
 int count=0;
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  if (freq<19 || freq>TIMER_FREQ) return 1;
+  if (freq<19 || freq>TIMER_FREQ/*1193182*/ || timer>2) return 1;
+
+  //Write control word to configure Timer 0:
   u_int8_t control;
-  if(timer_get_conf(timer, &control)!=0) return 1;
-  control=(control | 0x30) & 0x0F;
+  if(timer_get_conf(timer, &control)!=0) return 1;/
+  //ativa bits de LSB followed by MSB e retira os de read-back
+  control=(control | 0x30) & 0x3F;
+
+  //configura a control de acordo com o timer escolhido
   uint32_t option;
   switch (timer) {
     case 0:
@@ -32,6 +37,10 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
     default:
       return 1;
   }
+
+  /*Load timer’s register with the value of the divisor to
+    generate the frequency corresponding to the desired
+    rate*/
   uint32_t start= TIMER_FREQ/ freq;
   // os 16 bits do meio são perdidos, mas não serão usados pela configuração do sistema
   u_int8_t LSB, MSB;
@@ -45,6 +54,8 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   return 1;
 }
 
+
+//adiciona a subscrição à interrupção
 int (timer_subscribe_int)(uint8_t *bit_no) {
   if(bit_no!=NULL){
     if(sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hookId)==0){
@@ -56,6 +67,7 @@ int (timer_subscribe_int)(uint8_t *bit_no) {
   return 1;
 }
 
+//remove a subscrição à interrupção
 int (timer_unsubscribe_int)() {
   if(sys_irqrmpolicy(&hookId)==0)return 0;
   printf("Erro ao remover a interrupção\n");
@@ -66,6 +78,8 @@ void (timer_int_handler)() {
   count ++;
 }
 
+
+//dá a config de acordo com o timer dado
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   if(st==NULL || timer>2){
     return 1;
@@ -77,6 +91,8 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   return 0;
 }
 
+
+//dá display à config de acordo com a variável field
 int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
   union timer_status_field_val result;
 
