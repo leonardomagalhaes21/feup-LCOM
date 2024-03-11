bb610517fc3ef45e9bf5226a2dfaaca0a787c4db
Tiago Pinto <up202206280@up.pt>
2024-03-11 16:43:16 +0000

    another typo

--- lab2/utils.c
+++ lab2/utils.c
@@ -6,7 +6,7 @@ int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   if(lsb==NULL){
     return 1;
   }
-  *lsb=(u_int8_t)(val & 0xFF);
+  *lsb=(uint8_t)(val & 0xFF);
   return 0;
 }
 
@@ -14,7 +14,7 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   if(msb==NULL){
     return 1;
   }
-  *msb=(u_int8_t)((val >>8)& 0xFF);
+  *msb=(uint8_t)((val >>8)& 0xFF);
   return 0;
 }
 
@@ -24,6 +24,6 @@ int (util_sys_inb)(int port, uint8_t *value) {
   uint32_t temp;
   if(sys_inb(port,&temp)!=0)
     return 1;
-  *value=(u_int8_t)temp;
+  *value=(uint8_t)temp;
   return 0;
 }
